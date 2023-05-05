ba10b31bb6849eb1467adec6e1b66be6787ee0e7
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-05-05 12:58:55 -0600

    commit inicial

---
+++ src/app/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const statics = __dirname.replace("app", "public")
+
+app.set("port", process.env.PORT || 4000)
+app.set("views", "./src/public/view")
+app.set("view engine", "pug")
+
+app.use(morgan('dev'));
+app.use(express.static(statics));
+
+app.get('/', (req, res) => {
+    res.render("page/index")
+    res.end()
+})
+
+app.get('/contact', (req, res) => {
+    res.render("page/contact")
+})
+
+module.exports = app;
---
+++ src/public/js/scrolldown.js
@@ -0,0 +1,15 @@
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function () {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if (ubicacionPrincipal >= Desplazamiento_Actual) {
+        document.getElementById('navbar').style.top = '0';
+    }
+    else {
+        document.getElementById('navbar').style.top = '-110px';
+    window.addEventListener("scroll", function () {
+        var nav = document.querySelector("nav");
+        nav.classList.toggle("down", window.scrollY > 100);
+    })
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}

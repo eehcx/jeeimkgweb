e16291de91b9a7da316abc7f6bf9cee601b4c218
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-06-21 20:05:33 -0600

    Ejecución correcta de firebase

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const convertPugToHtml = require('./public/js/converter');
+
+// Convert Pug views to HTML
+convertPugToHtml();
+
 const app = require('./app/app');
 
 app.listen(app.get("port"), () => {
     console.log(`Server running on port ${app.get("port")}`);
-})
+});
